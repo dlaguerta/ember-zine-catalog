2c1699496eaf58d58f75e466ca6c88edeee78f84
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-10 19:19:59 -0800

    created basic zines route

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('zines');
 });
 
 export default Router;
---
+++ tests/unit/routes/zines-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:zines', 'Unit | Route | zines', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});

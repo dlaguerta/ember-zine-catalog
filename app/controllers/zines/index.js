d169c38654964e966bd37f1a4cfe947be0cf0947
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-12 11:10:26 -0800

    list filtering works, but now all zines does not

--- app/controllers/zines/index.js
+++ app/controllers/zines/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
-
+//import the controller from eariler
 export default Ember.Controller.extend({
+  actions: {
+   filterByTitle(param) {
+     if (param !== '') {
+       return this.get('store').query('zine', { title: param });
+     } else {
+       return this.get('store').findAll('zine');
+     }
+   }
+ }
 });

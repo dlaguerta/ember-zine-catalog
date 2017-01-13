e91c6c7580bb3c1cc4d5e76af35ca2d8f02f4906
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-13 08:38:26 -0800

    adding array transform to see if Ember will accept arrays

--- app/models/zine.js
+++ app/models/zine.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  creator: DS.attr('string')
+  creator: DS.attr('string'),
+  library_id: DS.attr( )
 });

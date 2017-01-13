d2a6f7dae7f26f0dd04dd7d010f95d6b05a67836
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-13 12:36:25 -0800

    adding fields from flywheel zines

--- app/models/zine.js
+++ app/models/zine.js
@@ -3,5 +3,15 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   creator: DS.attr('string'),
-  library_id: DS.attr( )
+  library_id: DS.attr( ),
+  sub_title: DS.attr('string'),
+  volume: DS.attr('number'),
+  number: DS.attr('number'),
+  date: DS.attr('string'),
+  circa_date: DS.attr('string'),
+  place_of_publication: DS.attr('string'),
+  tags: DS.attr('string'),
+  contributor: DS.attr('string'),
+  subject_genre: DS.attr('string'),
+  press_house_publisher: DS.attr('string'),
 });

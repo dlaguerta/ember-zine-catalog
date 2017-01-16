92eacc65511c4609a81a0423a95223ec057e9232
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-16 15:25:49 -0800

    minor change in zine route model for index, make sure it's not bad

--- app/routes/zines/index.js
+++ app/routes/zines/index.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('zine');
+    // return this.get('store').findAll('zine');
+    return Ember.RSVP.hash({
+     zines: this.store.findAll('zine'),
+     libraries: this.store.findAll('library')
+   });
   }
 });

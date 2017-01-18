9c1a3df0249a2f05aaf01f3433ea33fae5a0bee6
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-18 12:22:26 -0800

    changed to RSVP hash in application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function() {
-		return this.store.findAll('zine');
-	}
-});
+	// model: function() {
+	// 	return this.store.findAll('zine');
+	// }
+	model() {
+    return Ember.RSVP.hash({
+     zines: this.store.findAll('zine'),
+     libraries: this.store.findAll('library')
+   });
+ }});

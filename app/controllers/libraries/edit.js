96ee604b10aed38e20ea26792a71e57df0ad4183
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-18 15:06:17 -0800

    WIP submit action

--- app/controllers/libraries/edit.js
+++ app/controllers/libraries/edit.js
@@ -2,8 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-      save() {
+      save:function(model) {
         console.log('clicked button to save');
-      }
-   }
+        console.log(model);
+    }
+  }
 });
+// this.get('store').findRecord('library', params.library_id) .then(function(library) {
+//   library.get('name'); // => "Rails is Omakase"
+//
+//   library.set('name', 'A new library');
+//
+//   library.save(); // => PATCH to '/posts/1'
+// });
+// }
+// }

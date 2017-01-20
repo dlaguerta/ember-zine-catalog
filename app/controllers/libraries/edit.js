1e973c3f9234db3d4d2b042af83c055e9bb920a9
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-19 23:05:28 -0800

    removed some comments

--- app/controllers/libraries/edit.js
+++ app/controllers/libraries/edit.js
@@ -3,18 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
       saveLibrary: function() {
-        console.log('clicked button to save');
         console.log('after click:' + this.get("model").name);
         // var library = this.get("model");
         // console.log("LIBRARY" + library);
         // console.log(library.get('name'));
         // library.set('name', library.get('name'));
-        // this.get('model').updateRecord();
         this.get('model').save().then(function(){
-          console.log('library.save happened, then:');
+          console.log('library.save happens');
           this.transitionTo('libraries.show', this.get('model'));
         });
-        //need to figure out how to pass input here
     }
   }
 });
--- app/routes/libraries/edit.js
+++ app/routes/libraries/edit.js
@@ -2,8 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    console.log("HEY COOL DUDES WHATSUP");
-    console.log(params);
+    // console.log(params);
     return this.store.findRecord('library', params.library_id);
     // return this.modelFor('library');
   },
@@ -12,18 +11,6 @@ export default Ember.Route.extend({
      this._super(controller, model);
     controller.set("model", model);
   },
+  //try placing the action here if it does not activate in controller
 
-  // actions: {
-  //     saveLibrary(parameters) {
-  //
-  //       console.log("PARAMETERS:::" + parameters);
-  //       console.log("PARAMETERS:::" + parameters.get("name"));
-  //       parameters.changedAttributes();
-  //       parameters.save().then(function(){
-  //
-  //         console.log('success?');
-  //       });
-  //       //need to figure out how to pass input here
-  //   }
-  // }
 });

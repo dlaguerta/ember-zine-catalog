d91b9bcbc9d4f6ea95aa405dc5499ab7b2f3e4c2
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-20 12:52:52 -0800

    added an include to the models, WIP

--- app/routes/zines/index.js
+++ app/routes/zines/index.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('zine');
+    // return this.get('store').findAll('zine');
 
-    // return Ember.RSVP.hash({
-    //  zines: this.store.findAll('zine'),
-    //  libraries: this.store.findAll('library')
-  //  });
+    return Ember.RSVP.hash({
+     zines: this.store.findAll('zine', {include: 'library'}),
+     libraries: this.store.findAll('library')
+   });
   }
 });
--- app/routes/zines/show.js
+++ app/routes/zines/show.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').findRecord('zine', params.zine_id);
+    return this.get('store').findRecord('zine', params.zine_id, {include:'libraries'});
 }
 });

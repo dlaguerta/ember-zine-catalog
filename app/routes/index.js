93b4533dd997093556ac3968755b86781a7639e8
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-20 12:18:21 -0800

    added comment for redirect to zine catalog page from home

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel() {
-    //to add a redirect:
+    // to add a redirect to entire catalog:
     // this._super(...arguments);
     // this.replaceWith('zines');
   }

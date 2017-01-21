feb9a96318c6c189db6b924646c87964b307d3db
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-20 17:04:02 -0800

    added extractMeta to REST Adaptor to understand pagination on Ember side

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,11 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
 	namespace: 'api',
-	host: 'http://localhost:4500'
+	host: 'http://localhost:4500',
+	extractMeta: function(store, type, payload) {
+	 if (payload && payload.total) {
+		 store.setMetadataFor(type, { total: payload.total });  // sets the metadata for "post"
+		 delete payload.total;  // keeps ember data from trying to parse "total" as a record
+	 }
+ }
 });

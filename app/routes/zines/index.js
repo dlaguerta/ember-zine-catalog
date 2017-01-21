b0ab4651e60ce83fcd6327910808505ea6eaf260
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-20 18:42:48 -0800

    WIP pagination

--- app/routes/zines/index.js
+++ app/routes/zines/index.js
@@ -5,7 +5,9 @@ export default Ember.Route.extend({
     // return this.get('store').findAll('zine');
 
     return Ember.RSVP.hash({
-    zines: this.store.findAll('zine'),
+    zines: this.store.query('zine', {limit: 10}),
+    //original line: 
+    // zines: this.store.findAll('zine'),
     //with metadata changes
     //  zines: this.store.findAll('zine', {limit: 10, offset: 10}),
     //  libraries: this.store.findAll('library')

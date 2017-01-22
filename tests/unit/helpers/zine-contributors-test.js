b3ac1a0a2f9c0a888c556e4b4bfbf4d159fb22b3
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-22 12:24:39 -0800

    added helper(WIP), added if statement for showing contributors when applicable

---
+++ tests/unit/helpers/zine-contributors-test.js
@@ -0,0 +1,12 @@
+
+import { zineContributors } from 'ember-mongo/helpers/zine-contributors';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | zine contributors');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = zineContributors([42]);
+  assert.ok(result);
+});
+

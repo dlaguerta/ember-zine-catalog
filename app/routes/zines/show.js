133d389e0add36361837442cda2107a29a8c9ae2
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-22 15:34:28 -0800

    lost show redirect

--- app/routes/zines/show.js
+++ app/routes/zines/show.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').findRecord('zine', params.zine_id, {include:'libraries'});
+    return this.get('store').findRecord('zine', params.zine_id);
+    // return this.get('store').findRecord('zine', params.zine_id, {include:'libraries'});
 }
 });

9eba2d0cb2bcde3fe8406267d50aa11e7bd9f5d4
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-23 11:10:36 -0800

    added missing semicolon

--- app/helpers/zine-contributors.js
+++ app/helpers/zine-contributors.js
@@ -1,12 +1,11 @@
 import Ember from 'ember';
-const libraryIDS = ['58838840b4150dbb7e26c3cc']
+const libraryIDS = ['58838840b4150dbb7e26c3cc'];
 export function zineContributors([libraries]/*, hash*/) {
-  
+
   if (libraryIDS.includes(libraries)) {
     return 'Flywheel Zine Library';
-  }
-  console.log(libraries);
-  return 'Queer Zine Archive Project';
+    }
+    return 'Queer Zine Archive Project';
 }
 
 export default Ember.Helper.helper(zineContributors);

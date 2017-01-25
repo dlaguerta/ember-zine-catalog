539e8bde5aef522b5d07526fe259f4d9f18673b3
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-24 21:31:01 -0800

    displaying default photo for all qzap zines

--- app/routes/zines/index.js
+++ app/routes/zines/index.js
@@ -2,15 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    // return this.get('store').findAll('zine');
-
     return Ember.RSVP.hash({
-    // zines: this.store.query('zine', {limit: 10}),
-    //original line:
-    zines: this.store.findAll('zine'),
-    //with metadata changes
-    //  zines: this.store.findAll('zine', {limit: 10, offset: 10}),
-    //  libraries: this.store.findAll('library')
+      zines: this.store.findAll('zine'),
+
    });
   }
 });
